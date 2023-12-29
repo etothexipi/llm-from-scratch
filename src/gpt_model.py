deda5ee47725415d218efa53d0941bb11a4db9e8
Jonathan Sims <jonathan.sims@pm.me>
2023-12-29 12:12:34 -0800

    tried encoder-only with attention mask, still only predicting commas

--- src/gpt_model.py
+++ src/gpt_model.py
@@ -1,11 +1,58 @@
+import math
+
 import torch
 from torch.cuda.amp import GradScaler, autocast
 import torch.nn as nn
 import torch.optim as optim
-from torch.profiler import profile, ProfilerActivity
+# from torch.profiler import profile, ProfilerActivity
 
 
-class GPTModel(nn.Module):
+# Class taken from: https://github.com/pytorch/examples/blob/30b310a977a82dbfc3d8e4a820f3b14d876d3bd2/word_language_model/model.py#L65C1-L105C31
+class PositionalEncoding(nn.Module):
+    """Inject some information about the relative or absolute position of the tokens in the sequence.
+        The positional encodings have the same dimension as the embeddings, so that the two can be summed.
+        Here, we use sine and cosine functions of different frequencies.
+    .. math:
+        \text{PosEncoder}(pos, 2i) = sin(pos/10000^(2i/d_model))
+        \text{PosEncoder}(pos, 2i+1) = cos(pos/10000^(2i/d_model))
+        \text{where pos is the word position and i is the embed idx)
+    Args:
+        d_model: the embed dim (required).
+        dropout: the dropout value (default=0.1).
+        max_len: the max. length of the incoming sequence (default=5000).
+    Examples:
+        >>> pos_encoder = PositionalEncoding(d_model)
+    """
+
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        r"""Inputs of forward function
+        Args:
+            x: the sequence fed to the positional encoder model (required).
+        Shape:
+            x: [batch size, sequence length, embed dim]
+            output: [batch size, sequence length, embed dim]
+        Examples:
+            >>> output = pos_encoder(x)
+        """
+        # Need to adjust the tensor shape to match other modules (batch_size, seq_len, embed_dim)
+        x = x.transpose(0, 1)
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x.transpose(0, 1))
+    
+
+class GPTModel(nn.Transformer):
     """
     A PyTorch module for a GPT-like transformer model.
 
@@ -22,11 +69,16 @@ class GPTModel(nn.Module):
         :param num_heads: int, the number of heads in the multiheadattention models.
         :param num_layers: int, the number of sub-encoder-layers in the transformer.
         """
-        super().__init__()
+        super().__init__(d_model=output_dim, nhead=num_heads, num_encoder_layers=num_layers)
+        self.output_dim = output_dim
+        # self.num_heads = num_heads
+        # self.num_layers = num_layers
         self.token_embedding_layer = nn.Embedding(vocab_size, output_dim)
-        self.pos_embedding_layer = nn.Embedding(block_size, output_dim)
-        self.transformer = nn.Transformer(d_model=output_dim, nhead=num_heads, num_encoder_layers=0, num_decoder_layers=num_layers, batch_first=True)
-        self.linear = nn.Linear(output_dim, vocab_size)
+        # self.pos_embedding_layer = nn.Embedding(block_size, output_dim)
+        # self.transformer_decoder_layer = nn.TransformerDecoderLayer(d_model=output_dim, nhead=num_heads)
+        # self.transformer_decoder = nn.TransformerDecoder(self.transformer_decoder_layer, num_layers=num_layers)
+        self.pos_encoder = PositionalEncoding(d_model=output_dim, dropout=0.0, max_len=block_size)
+        self.linear_decoder = nn.Linear(output_dim, vocab_size)
 
     def forward(self, inputs):
         """
@@ -35,17 +87,14 @@ class GPTModel(nn.Module):
         :param inputs: torch.Tensor, input tensor.
         :return: torch.Tensor, logits output by the model.
         """
-        token_embeddings = self.token_embedding_layer(inputs)
-        batch_size, seq_length, _ = token_embeddings.shape
-        pos_embeddings = self.pos_embedding_layer(torch.arange(seq_length, device=inputs.device))
-        input_embeddings = token_embeddings + pos_embeddings
-        # Creating a dummy input for the encoder part
-        dummy_src = torch.zeros_like(input_embeddings, device=inputs.device)
-        # Creating a mask that will effectively ignore the encoder input
-        src_mask = torch.ones((batch_size, seq_length), device=inputs.device).bool()
-
-        transformer_out = self.transformer(src=dummy_src, tgt=input_embeddings, src_mask=src_mask)
-        logits = self.linear(transformer_out)
+        # _batch_size, seq_length, _embedding_dimensions = token_embeddings.shape
+        # pos_embeddings = self.pos_embedding_layer(torch.arange(seq_length, device=inputs.device))
+        # input_embeddings = token_embeddings + pos_embeddings
+        token_embeddings = self.token_embedding_layer(inputs) * math.sqrt(self.output_dim)
+        src = self.pos_encoder(token_embeddings)
+        mask = torch.log(torch.tril(torch.ones(len(src),len(src)))).to(inputs.device)
+        output = self.encoder(src, mask=mask)
+        logits = self.linear_decoder(output)
         # print(logits)
         return logits
 
@@ -78,10 +127,10 @@ class GPTModel(nn.Module):
                 inputs, targets = inputs.pin_memory().to(device, non_blocking=True), targets.pin_memory().to(device, non_blocking=True)
 
                 # Mixed precision training
-                # with autocast():
-                logits = self(inputs)
-                loss = criterion(logits.view(-1, logits.size(-1)), targets.view(-1))
-                scaler.scale(loss).backward()
+                with autocast():
+                    logits = self(inputs)
+                    loss = criterion(logits.view(-1, logits.size(-1)), targets.view(-1))
+                    scaler.scale(loss).backward()
                 
                 # Gradient accumulation
                 if (step + 1) % grad_accumulation_steps == 0:
